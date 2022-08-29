127b084bb0889c5a030a52eaa2c65f69312748ff
“Jenniferhubermayer” <jennifer.huber.mayer@gmail.com>
2022-08-29 14:27:11 +0200

    SCSS Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -179,7 +179,6 @@ showArticles();
 
 // ACCORDION
 var accordion = document.getElementsByClassName("accordion");
-// var i;
 
 for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", function() {
